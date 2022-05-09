7b7be3a821edfdba5d3e66699bd01be6266bb106
JosefinaOller <64923768+JosefinaOller@users.noreply.github.com>
2022-05-08 23:41:24 -0300

    Corrigiendo unos detalles.
    Aún así siguen sin aparecer los pescados.

--- producto/scenes/Enemigo_Lobo.gd
+++ producto/scenes/Enemigo_Lobo.gd
@@ -1,16 +1,9 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.playing = true
-	var mob_types = $AnimatedSprite.frames.get_animation_names()
-	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	var fish_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = fish_types[randi() % fish_types.size()]
 
 
 func _on_VisibilityNotifier2D_screen_exited():
--- producto/scenes/Main_Lobo.gd
+++ producto/scenes/Main_Lobo.gd
@@ -4,17 +4,10 @@ extends Node2D
 export(PackedScene) var fish_scene
 var score
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	randomize()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_Player_hit():
-	pass # Replace with function body.
+	
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()

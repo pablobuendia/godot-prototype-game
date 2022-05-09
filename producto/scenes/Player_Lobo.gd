ea84e0684dfe42aa3618635d3f8f192555cbb43f
JosefinaOller <64923768+JosefinaOller@users.noreply.github.com>
2022-05-08 22:52:25 -0300

    Eliminación de escenas innecesarias y creación de escena Player_Lobo
    Se creó la escena de Player_Lobo donde el personaje principal es el lobo, por ahora solamente tiene movimiento hacia la derecha, tiene incorporada la colisión en el sprite para cuando se choque con los pescados. Todo eso siguiendo la documentación de Godot.

---
+++ producto/scenes/Player_Lobo.gd
@@ -0,0 +1,35 @@
+extends Area2D
+signal hit
+
+export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _on_Player_body_entered(body):
+	hide() 
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false

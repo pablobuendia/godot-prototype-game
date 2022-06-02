e41bd3b38e2257c71c0d7c9abcdaf0a74ad6b89e
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-06-02 08:59:31 -0300

    Se agrega estructura de datos y modos de juego. Se modifica puntaje de victoria

--- producto/Minijuegos/Pesca/Pesca-Main.gd
+++ producto/Minijuegos/Pesca/Pesca-Main.gd
@@ -1,37 +1,36 @@
 extends Node2D
+# TODO: Spawnear obstaculos que no sumen puntos (o que resten)
+# TODO: Agregar pantalla de inicio de juego
+# TODO: Complejizar calculo de puntaje
+# TODO: Agregar efectos de sonido a los golpes
+# TODO: Parametrizar velocidades, spawn times, poderes del bote
+# TODO: Agregar al menos tres modos de dificultad
+# TODO: Limpiar y organizar codigo, extraer constantes y renombrar variables
+# TODO: Agregar modo de pausa y de guardado de partida (aclarar una forma en comun)
+# TODO: Redefinir controles touchscreen
+# TODO: Limitar numero de ganchos spawneables (ahora son tantos como el jugador genere)
+# TODO: Definir al menos otro poder (super anzuelo con x10 velocidad o lluvia de anzuelos)
 # TODO(X): Separar escena Bote del nodo Main
 # TODO(X): Generar diferentes escalas de pez
 # TODO(X): Encontrar forma de reusar misma escena pez pero con diferentes sprites
-# TODO: Spawnear obstaculos que no sumen puntos (o que resten)
 # TODO(X): Crear disparador de red y de caña de pescar
 # TODO(X): Generar acumulador de puntaje
-# TODO: Agregar pantalla de inicio de juego
 # TODO(X): Agregar pantalla de fin de juego
 # TODO(X): Investigar Modo twin
 # TODO(X): Parametrizar modos de juego (más/menos peces, más menos obstáculos, etc)
 # TODO(X): Control para touch screen: derecha, izquierda, caña
 # TODO(X): Agregar musica
-# TODO: Agregar efectos de sonido a los golpes
 # TODO(X): Agregar modo y musica de victoria
-# TODO: Parametrizar velocidades, spawn times, poderes del bote
-# TODO: Agregar al menos tres modos de dificultad
 # TODO(X): Animar movimiento de peces y de pesca
 # TODO(X): Ajustar colisiones entre anzuelos y peces
-# TODO: Limpiar y organizar codigo, extraer constantes y renombrar variables
-# TODO: Agregar modo de pausa y de guardado de partida (aclarar una forma en comun)
-# TODO: Redefinir controles touchscreen
-# TODO: Limitar numero de ganchos spawneables (ahora son tantos como el jugador genere)
-# TODO: Definir al menos otro poder (super anzuelo con poderes o lluvia de anzuelos)
 
 export(PackedScene) var pez_scene
-export var puntaje = 0
-export var PUNTAJE_VICTORIA = 100
+export var puntaje_actual = 0
+export var PUNTAJE_VICTORIA = 1000
 var VICTORIA = 0
-var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Comienza el juego...")
 	randomize()
 	new_game()
 
@@ -40,24 +39,27 @@ func _process(delta):
 	pass
 
 func get_puntaje():
-	return puntaje
+	return puntaje_actual
 	
 func set_puntaje(nuevo_puntaje):
 	if VICTORIA == 0:
-		puntaje = nuevo_puntaje
-		$Puntaje_valor.text = String(puntaje)
-		if puntaje >= PUNTAJE_VICTORIA:
+		puntaje_actual = nuevo_puntaje
+		
+		# Mostrar puntaje 
+		$Puntaje_valor.text = String(puntaje_actual)
+		if puntaje_actual >= PUNTAJE_VICTORIA:
 			VICTORIA = 1
 			$Label_victoria.visible = true
 			$Musica_victoria.play()
 
-func _on_Musica_victoria_finished():
-	get_tree().paused = true
-
 func new_game():
-	score = 0
+	puntaje_actual = 0
 	$Jugador.start($PosicionComienzo.position)
 	$StartTimer.start()
+
+# Señales
+func _on_Musica_victoria_finished():
+	get_tree().paused = true
 
 func _on_StartTimer_timeout():
 	$PezSpawnTimerDer.start()
